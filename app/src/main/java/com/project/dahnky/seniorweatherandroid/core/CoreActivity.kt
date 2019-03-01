b2e18788990450ddf1982265227f0efca9d81be3
kyledahn@gmail.com <kyledahn@gmail.com>
2019-03-01 00:10:45 -0600

    Added backNavigation and displayErrorDialog

--- app/src/main/java/com/project/dahnky/seniorweatherandroid/core/CoreActivity.kt
+++ app/src/main/java/com/project/dahnky/seniorweatherandroid/core/CoreActivity.kt
@@ -2,29 +2,35 @@ package com.project.dahnky.seniorweatherandroid.core
 
 import android.support.annotation.IdRes
 import android.support.v4.app.Fragment
+import com.afollestad.materialdialogs.MaterialDialog
 import dagger.android.support.DaggerAppCompatActivity
 
 abstract class CoreActivity : DaggerAppCompatActivity(), ICoreView {
 
-    override fun showProgress() {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun dismissProgress() {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
     override fun startFragment(@IdRes container: Int, fragment: Fragment, addToBackStack: Boolean) {
-        if (supportFragmentManager.findFragmentById(container) != null) {
+        if (supportFragmentManager.findFragmentById(container) != null)
             supportFragmentManager.beginTransaction().apply {
                 add(container, fragment)
             }.commit()
-        } else {
+        else
             supportFragmentManager.beginTransaction().apply {
                 replace(container, fragment)
                 if (addToBackStack)
                     addToBackStack(null)
             }.commit()
+    }
+
+    override fun backNavigation() {
+        if (supportFragmentManager.backStackEntryCount != 0)
+            supportFragmentManager.apply { popBackStack() }
+        else
+            apply { finish() }
+    }
+
+    override fun displayErrorDialog(content: String, enableRetry: Boolean) {
+        MaterialDialog(this).show {
+            title(text = "Error")
+            message(text = content)
         }
     }
 }
--- app/src/main/java/com/project/dahnky/seniorweatherandroid/core/ICoreView.kt
+++ app/src/main/java/com/project/dahnky/seniorweatherandroid/core/ICoreView.kt
@@ -4,7 +4,7 @@ import android.support.annotation.IdRes
 import android.support.v4.app.Fragment
 
 interface ICoreView {
-    fun showProgress()
-    fun dismissProgress()
     fun startFragment(@IdRes container: Int, fragment: Fragment,addToBackStack: Boolean)
+    fun backNavigation()
+    fun displayErrorDialog(content: String, enableRetry: Boolean)
 }
